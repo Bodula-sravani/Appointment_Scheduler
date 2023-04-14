c04552fcff9d6fd54528d085cead22d704c31ccd
Bodula-sravani <sravanibodula@gmail.com>
2023-04-14 14:32:48 +0530

    Add login,register&user pages

---
+++ Models/Appointment.cs
@@ -0,0 +1,16 @@
+﻿namespace Appointment_Scheduler.Models
+{
+	public class Appointment
+	{
+
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public DateTime Date { get; set;}
+		public TimeOnly StartTime { get; set; }
+		public TimeOnly EndTime { get; set; }
+
+		public string userId { get; set; }
+
+	}
+}
---
+++ Models/Users.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace Appointment_Scheduler.Models
+{
+	public class Users
+	{
+		public string userId { get; set; }
+
+		public string Name { get; set; }
+
+		public string Email { get; set; }
+
+		public string Password { get; set; }	
+
+		public string phoneNumber { get; set; }
+	}
+}
