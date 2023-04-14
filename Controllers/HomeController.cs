3d4500aceae7bc085561fe3737a5049fd670b7b5
Bodula-sravani <sravanibodula@gmail.com>
2023-04-14 14:32:48 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using Appointment_Scheduler.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Diagnostics;
+
+namespace Appointment_Scheduler.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+		IConfiguration configuration;
+		public SqlConnection connection;
+		public HomeController(IConfiguration configuration)
+		{
+			this.configuration = configuration;
+			this.connection = new SqlConnection(configuration.GetConnectionString("DB"));
+
+		}
+		//public HomeController(ILogger<HomeController> logger)
+  //      {
+  //          _logger = logger;
+  //      }
+
+        public bool validateUser(string userId,string password)
+        {
+			Console.WriteLine("entered validateUser method");
+			try
+			{
+				connection.Open();
+				SqlCommand command = new SqlCommand("getUser", connection);
+				command.CommandType = System.Data.CommandType.StoredProcedure;
+				command.Parameters.AddWithValue("@userid", userId);
+				SqlDataReader reader = command.ExecuteReader();
+				Console.WriteLine("reader excecuted");
+				string getPassword="";
+				while (reader.Read())
+				{
+					getPassword = (string)reader["userPassword"];
+				}
+				//Console.WriteLine("reader value: " + (string)reader["userPassword"]);
+				Console.WriteLine("passsowrds:  " + getPassword + " " + password);
+				if(password.Equals(getPassword))
+				{
+					Console.WriteLine("paswoord are equal");
+					return true;
+				}
+				reader.Close();
+				connection.Close();
+			}
+			catch(SqlException  ex) 
+			{
+				Console.WriteLine("error: " + ex.Message);
+			}
+			
+			Console.WriteLine("passwords not equal exit");
+			return false;
+		}
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+
+        public IActionResult Index(string userid,string password,Users user)
+        {
+			try
+			{
+				Console.WriteLine("in post index: ");
+				Console.WriteLine("userid: " + userid);
+				Console.WriteLine("password: " + password);
+				Console.WriteLine("user.id: " + user.userId);
+				if (validateUser(userid, password))
+				{
+					Console.WriteLine("validaed user");
+					//Console.WriteLine(user.Name);
+					return RedirectToAction("userPage","User",user.userId);
+				}
+				else
+				{
+					Console.WriteLine("paswword mismatch");
+					return View(); 
+				}
+
+			}
+			catch
+			{
+				return View();
+			}
+		}
+		public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,181 @@
+﻿using Appointment_Scheduler.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using NuGet.Protocol.Plugins;
+
+namespace Appointment_Scheduler.Controllers
+{
+	public class UserController : Controller
+	{
+
+		IConfiguration configuration;
+		public SqlConnection connection;
+		public UserController(IConfiguration configuration)
+		{
+			this.configuration = configuration;
+			this.connection = new SqlConnection(configuration.GetConnectionString("DB"));
+
+		}
+		// GET: UserController
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		// GET: UserController/Details/5
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
+
+		// GET: UserController/Create
+		public ActionResult Register()
+		{
+			return View();
+		}
+		public void insertUser(Users user)
+		{
+			Console.WriteLine("inside insert user");
+			try
+			{
+				connection.Open();
+				SqlCommand command = new SqlCommand("insertUser", connection);
+				command.CommandType = System.Data.CommandType.StoredProcedure;
+				command.Parameters.AddWithValue("@userid", user.userId);
+				command.Parameters.AddWithValue("@username", user.Name);
+				command.Parameters.AddWithValue("@useremail", user.Email);
+				command.Parameters.AddWithValue("@userpassword", user.Password);
+				command.Parameters.AddWithValue("@userphonenumber", user.phoneNumber);
+
+				command.ExecuteNonQuery();
+				connection.Close();
+			}
+			catch(SqlException e)
+			{
+				Console.WriteLine("error: " + e.Message);
+			}
+			Console.WriteLine(user.phoneNumber);
+			Console.WriteLine("exiting insert user");
+		}
+		// POST: UserController/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Register(Users user)
+		{
+			try
+			{
+				Console.WriteLine("phone nmber: "+user.phoneNumber);
+				insertUser(user);
+				Console.WriteLine("compeleted insert");
+				//return RedirectToAction("Home/Index");
+				return RedirectToAction("Index", "Home");
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		public Users getUser(string userId)
+		{
+            Console.WriteLine("entered getUser method");
+            Users user = new Users();
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("getUser", connection);
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@userid", userId);
+                SqlDataReader reader = command.ExecuteReader();
+                Console.WriteLine("reader excecuted");
+                while (reader.Read())
+                {
+                    user.Password = (string)reader["userPassword"];
+					user.phoneNumber = (string)reader["userPhonenumber"];
+					user.userId = (string)reader["userId"];
+					user.Email = (string)reader["userEmail"];	
+                }
+                reader.Close();
+                connection.Close();
+                
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("error: " + ex.Message);
+            }
+            return user;
+        }
+		public ActionResult userPage(string userId)
+		{
+			
+			Users user = getUser(userId);
+			ViewData["name"] = user.Name;
+            Console.WriteLine("user Name: " + user.Name);
+            return View(user);
+		}
+
+		public ActionResult Create(int id)
+		{
+			return View();
+		}
+
+		// POST: UserController/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Create(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+
+		// GET: UserController/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
+
+		// POST: UserController/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: UserController/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
+
+		// POST: UserController/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
+}
